4956d2c28fd397869d14540da911137e7468ea38
StarkTheGnr <lordsomy@hotmail.com>
2021-09-12 19:52:02 +0200

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data-driven solution to students not knowing the skills needed for a specific job',
+    author='Assem Younis',
+    license='',
+)
